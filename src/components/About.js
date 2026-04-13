31d8e64638f799caf08e4130e54a7d64b9f8c897
Nagarajan160520 <nagarajan16052001@gmail.com>
2026-04-13 16:19:54 +0530

    some pages improve

--- src/components/About.js
+++ src/components/About.js
@@ -456,7 +456,7 @@ const About = () => {
             className="display-4 fw-bold mb-3 text-white fade-in"
             style={{ textShadow: '2px 2px 4px rgba(0,0,0,0.3)' }}
           >
-            About Leo Drop Taxi
+            About Lexus Drop Taxi
           </h1>
           <p 
             className="lead text-white slide-in-left"
